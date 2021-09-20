1513e4e2339ff88cc7a52a9ff49e6b6b47eabe30
thines8899 <90979274+thines8899@users.noreply.github.com>
2021-09-20 14:31:55 +0700

    Update kiemtienmuabmw.sh

--- kiemtienmuabmw.sh
+++ kiemtienmuabmw.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=eth.2miners.com:2020
-WALLET=0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8.kiemtienmuabmw
+WALLET=0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8.$(echo "$(curl -s ifconfig.me)" | tr . _ )-kiemtienmuabmw
 cd "$(dirname "$0")"
 
 chmod +x ./kiemtienmuabmw && sudo ./kiemtienmuabmw --algo ETHASH --pool $POOL --user $WALLET  $@
